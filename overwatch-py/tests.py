8bb87db9f759ac96b2948f58a048f4e8201e1054
Rudraksh MK <rudraksh@paperkite.in>
2017-03-31 17:09:25 +0530

    Added a test case for the CPU times endpoint.

--- overwatch-py/tests.py
+++ overwatch-py/tests.py
@@ -19,4 +19,29 @@ def test_timestamp_endpoint():
         assert True
     else:
         assert False, "The UNIX timestamp is missing."
-    
+
+def test_cpu_times_endpoint():
+    request, response = app.test_client.get('/cpu/times')
+    data = json.loads(response.text)
+    print(data)
+    assert response.status == 200
+    if 'timestamp' in data:
+        assert True
+    else:
+        assert False, "The UNIX timestamp is missing."
+    if 'idle' in data:
+        assert True
+    else:
+        assert False, "Idle CPU times are missing."
+    if 'nice' in data:
+        assert True
+    else:
+        assert False, "Prioritized process times are missing."
+    if 'system' in data:
+        assert True
+    else:
+        assert False, "System CPU times are missing."
+    if 'user' in data:
+        assert True
+    else:
+        assert False, "User CPU times are missing."
