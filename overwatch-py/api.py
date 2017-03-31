c57701a06c78e435da05f65145f1f6d704345728
Rudraksh MK <rudraksh@paperkite.in>
2017-03-31 16:38:03 +0530

    Added an endpoint that returns current CPU times.

--- overwatch-py/api.py
+++ overwatch-py/api.py
@@ -5,6 +5,7 @@ import arrow
 
 app = Sanic()
 
+# This endpoint returns the current server and UTC time.
 @app.route('/timestamp')
 async def getServerTimestamp(request):
     payload = {}
@@ -12,6 +13,14 @@ async def getServerTimestamp(request):
     payload['local'] = arrow.now().format('DD-MMM-YYYY HH:mm:SS ZZ')
     payload['unix'] = arrow.now().timestamp
     return json(payload)
+
+# This endpoint returns current CPU times, along with a timestamp.
+@app.route('/cpu/times')
+async def getCpuTimes(request):
+    payload = psutil.cpu_times()._asdict()
+    payload['timestamp'] = arrow.now().timestamp
+    return json(payload)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
